6fb84a7de59579947c2d546ec857e4513b54b7a8
linxingjian <linxingjian199205@163.com>
2019-01-16 20:41:53 +0800

    test:  ci run command

--- .circleci/citest.sh
+++ .circleci/citest.sh
@@ -7,9 +7,15 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o xtrace
 
-curl --user "${CIRCLE_TOKEN}": \
-    --request POST \
-    --form revision=a0a0fff7bdaf84f95400d79eb7367ebe4684e8dc\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/smallcosmos/prepare-commit/tree/master
+echo `dirname $0`
+echo `pwd`
+echo `node --version`
+
+# git status
+
+# curl --user "${CIRCLE_TOKEN}": \
+#     --request POST \
+#     --form revision=a0a0fff7bdaf84f95400d79eb7367ebe4684e8dc\
+#     --form config=@config.yml \
+#     --form notify=false \
+#         https://circleci.com/api/v1.1/project/github/smallcosmos/prepare-commit/tree/master
